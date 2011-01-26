25d90ebcd96b35cad3e2464fad23ac2082c288ad
Tom Davidson <tjd@stanford.edu>
2011-01-25 17:43:59 -0800

    initial commit of Chronux package to this repo

---
+++ chronux_pathdef.m
@@ -0,0 +1,20 @@
+function p = chronux_pathdef
+% LOCAL_PATHDEF - some paths you might want - chronux
+%
+% In your startup.m: 
+%
+%    cd ~/hg/chronux % path to this folder containing this file
+%    path(chronux_pathdef,p)
+%
+
+if isunix
+    path_to_repo = '/home/tjd/hg/chronux';
+else
+    path_to_repo = 'X:/chronux';
+end
+
+p = [...
+    genpath([path_to_repo '/spectral_analysis']) ...
+    genpath([path_to_repo '/dataio']) ...
+    genpath([path_to_repo '/wave_browser']) ...
+    genpath([path_to_repo '/spikesort'])];
